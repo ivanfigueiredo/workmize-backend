e0345ca34b789e6912a1d58c2e3116bb2a136898
ivanfigueiredo <77892424+ivanfigueiredo@users.noreply.github.com>
2021-11-12 09:21:54 -0300

    Delete server.ts

--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import {App} from './app';
-
-const app = new App();
-const server = app.Express();
-
-server.listen(`${process.env.PORT}`, () => {
-    console.log(`Server is runing on... ${process.env.BASE}:${process.env.PORT}`);
-    
-});
