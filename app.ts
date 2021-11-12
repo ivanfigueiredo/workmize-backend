0963da46d076fd532ae63a74570d3cedfe8c9a8c
ivanfigueiredo <77892424+ivanfigueiredo@users.noreply.github.com>
2021-11-12 09:51:48 -0300

    Update app.ts

--- app.ts
+++ app.ts
@@ -3,7 +3,7 @@ import {App} from './server';
 const app = new App();
 const server = app.Express();
 
-const port = `${process.env.PORT}` || 8080;
+const port = `${process.env.PORT}` || 3000;
 
 server.listen(port, () => {
     console.log(`Server is runing on... ${process.env.BASE}:${process.env.PORT}`);
